683bc4f7d0f21edb658520b96db45cd74ca2ddca
KyleLambdin78 <43045070+KyleLambdin78@users.noreply.github.com>
2020-05-13 16:48:27 -0400

    Drag and Drop
    Drag and Drop code as well as basic item slots created

---
+++ Potion Shop/Assets/Scripts/DragDrop.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    private Vector3 defaultPos;
+
+    // Onslot is to make sure that the dropped object is placed on an Item Slot
+    [HideInInspector] public bool onSlot;
+    [SerializeField] private Canvas canvas;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        defaultPos = rectTransform.localPosition;
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 0.6f;
+        canvasGroup.blocksRaycasts = false;
+        onSlot = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 1.0f;
+        canvasGroup.blocksRaycasts = true;
+        if (onSlot == false)
+        {
+            rectTransform.anchoredPosition = defaultPos;
+        }
+        else
+        {
+            defaultPos = rectTransform.anchoredPosition;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        
+    }
+
+  
+}
+
