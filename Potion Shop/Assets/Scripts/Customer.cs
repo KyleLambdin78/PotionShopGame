f86ecec3892157ca83d18ffda5ebd6c5dcd46fe7
KyleLambdin78 <43045070+KyleLambdin78@users.noreply.github.com>
2020-05-20 17:52:48 -0400

    Customers almost done
    Customers Object Pooled with timers

--- Potion Shop/Assets/Scripts/Customer.cs
+++ Potion Shop/Assets/Scripts/Customer.cs
@@ -2,65 +2,179 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class Customer : MonoBehaviour
+using UnityEngine.EventSystems;
+public class Customer : MonoBehaviour, IDropHandler
 {
     public GameObject thoughtBubble;
     public float speed;
     public float distance;
     public Transform detection;
+    public Vector3 defPosition;
     public Image potion;
+    public Text timerText;
     public PotionClass[] potionClass;
-    private bool atCounter;
+    private float startTime;
+    private PotionClass wantedPotion;
+    private GameManager GameManager;
+    private enum State {ApproachingCounter, AtCounter, OrderPlaced, OrderReceived}
+    State customerState = State.ApproachingCounter;
     void Start()
     {
-        PickPotion();
-        //StartCoroutine(MoveRight());
+        timerText.enabled = false;
+        startTime = 0;
+        GameManager = FindObjectOfType<GameManager>();
+        defPosition = this.transform.position;
+        ResetCustomer();
+        StartCoroutine(MoveRight());
     }
-    public void Update()
-    {
-        
-        RaycastHit2D rightHit = Physics2D.Raycast(detection.position, Vector2.right, distance);
-        if (rightHit.collider == false)
-        {
-            transform.Translate(Vector2.right * speed * Time.deltaTime);
-        }
-        else
-        {
 
-        }
-        
-    }
     public void PickPotion()
     {
-        PotionClass wantedPotion;
         wantedPotion = potionClass[Random.Range(0, potionClass.Length)];
         potion.sprite = wantedPotion.potionSprite;
         thoughtBubble.SetActive(true);
     }
     public void CustomerServed()
     {
-        thoughtBubble.SetActive(false);
-        StartCoroutine(MoveDown());
+        GameManager.CreateCustomers();
+        if(customerState == State.AtCounter)
+        {
+            thoughtBubble.SetActive(false);
+            customerState = State.OrderPlaced;
+            StartCoroutine(MoveDown());
+        }
+        else if(customerState == State.OrderPlaced)
+        {
+            customerState = State.OrderReceived;
+            StartCoroutine(MoveDown());
+        }
+        
     }
     IEnumerator MoveDown()
     {
-        yield return true;
+        while (customerState == State.OrderPlaced || customerState == State.OrderReceived)
+        {
+            RaycastHit2D rightHit = Physics2D.Raycast(detection.position, Vector2.down, distance);
+            if (rightHit.collider == false)
+            {
+                transform.Translate(Vector2.down * speed * Time.deltaTime);
+            }
+            else if (rightHit.collider == true)
+            {
+                if(customerState == State.OrderPlaced)
+                {
+                    StartCoroutine(MoveRight());
+                    yield break;
+                }
+                else if (customerState == State.OrderReceived)
+                {
+                    this.transform.position= defPosition;
+                    ResetCustomer();
+                    RestartCustomer();
+                    yield break;
+                }
+               
+            }
+
+            yield return null;
+        }
     }
     IEnumerator MoveRight()
     {
-        RaycastHit2D rightHit = Physics2D.Raycast(detection.position, Vector2.right, distance);
-        while (atCounter == false)
+      while(customerState == State.ApproachingCounter || customerState == State.OrderPlaced)
         {
-            
-            if(rightHit.collider == true)
+            RaycastHit2D rightHit = Physics2D.Raycast(detection.position, this.transform.right, distance);
+            if (rightHit.collider == false)
             {
                 transform.Translate(Vector2.right * speed * Time.deltaTime);
-                atCounter = true;
-                yield return null;
             }
-            
+            else if (rightHit.collider == true)
+            {
+                if(customerState == State.ApproachingCounter)
+                {
+                    customerState = State.AtCounter;
+                    thoughtBubble.SetActive(true);
+                    StartCoroutine(Timer());
+                    yield break;
+                }
+                else if (customerState == State.OrderPlaced)
+                {
+                    thoughtBubble.SetActive(true);
+                    yield break;
+                }
+                yield return true;
+            }
+
+            yield return null;
         }
         
 
+    }
+    public void ResetCustomer()
+    {
+        PickPotion();
+        thoughtBubble.SetActive(false);
+    }
+    public void RestartCustomer()
+    {
+        customerState = State.ApproachingCounter;
+        StartCoroutine(MoveRight());
+    }
+    public void OnDrop(PointerEventData eventData)
+    {
+        GameObject givenPotion = eventData.pointerDrag;
+        if(givenPotion.GetComponent<PotionDisplay>().potion == wantedPotion && customerState == State.OrderPlaced)
+        {
+            customerState = State.OrderReceived;
+            GameManager.AddGold(100);
+            Destroy(givenPotion);
+            StartCoroutine(MoveDown());
+        }
+        else if(givenPotion.GetComponent<PotionDisplay>(). potion != wantedPotion && customerState == State.OrderPlaced)
+        {
+            Destroy(givenPotion);
+            Debug.Log("Wrong Potion");
+            StartCoroutine(MoveDown());
+        }
+       
+      
+    }
+    public IEnumerator Timer()
+    {
+        timerText.enabled = true;
+        startTime = Time.time;
+        while(customerState != State.OrderReceived)
+        {
+            float t = Time.time - startTime;
+
+            string minutes = ((int)t / 60).ToString();
+            string seconds = (t % 60).ToString("f1");
+            if(t >= 60)
+            {
+                timerText.text = minutes + ":" + seconds;
+            }
+            else
+            {
+                timerText.text = seconds;
+            }
+            
+            if(t < 10)
+            {
+                timerText.color = Color.green;
+            }
+            else if(t > 10 && t < 20)
+            {
+                timerText.color = Color.yellow;
+            }
+            else
+            {
+                timerText.color = Color.red;
+            }
+            yield return null;
+        }
+        yield return new WaitForSeconds(2);
+        startTime = Time.time;
+        timerText.enabled = false;
+        yield break;
     }
 }
---
+++ Potion Shop/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public GameObject spawnSpot;
+    public Canvas canvas;
+    public Text goldText;
+    public float goldAmount = 0;
+    public GameObject customer;
+    public int customerCount = 3;
+    private List<GameObject> currentCustomers = new List<GameObject>();
+    
+    void Start()
+    {
+        goldText.text = "Gold:" + goldAmount;
+        CreateCustomers();
+    }
+
+    public void CreateCustomers()
+    {
+        if(currentCustomers.Count < customerCount)
+        {
+            GameObject spawnedCustomer = Instantiate(customer, new Vector3(spawnSpot.transform.position.x, spawnSpot.transform.position.y, spawnSpot.transform.position.z), Quaternion.identity, canvas.transform);
+            currentCustomers.Add(spawnedCustomer);
+        }
+        else
+        {
+            CallRestartCustomer();
+        }
+    }
+    public void AddGold(float gold)
+    {
+        goldAmount += gold;
+        goldText.text = "Gold:" + goldAmount;
+    }
+    public void CallRestartCustomer()
+    {
+       
+    }
+    public void MoveLine()
+    {
+        foreach (GameObject customer in currentCustomers)
+        {
+            customer.GetComponent<Customer>().StartCoroutine("MoveRight()");
+        }
+    }
+}
